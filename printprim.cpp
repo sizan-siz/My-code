4d105380751ae7c3c7b437c7975c669fdeb34b7b
sizan-siz <111038228+sizan-siz@users.noreply.github.com>
2022-08-12 12:00:16 +0600

    Add files via upload

---
+++ printprim.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,i,j;
+    cout<<"Enter your number:\n";
+    cin>>num;
+    for(i=2;i<=num;i++)
+    {
+        for( j=2;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                break;
+            }
+        }
+        if(i==j)
+        {
+            cout<<j<<endl;
+            j++;
+        }
+    }
+}
